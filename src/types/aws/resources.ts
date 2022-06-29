a5813330cd73d2881579a1657f9e8492e53e3934
Bohdan Tkachuk <b.tkachuk26@gmail.com>
2022-06-29 18:42:46 +0300

    format script change

--- src/types/aws/resources.ts
+++ src/types/aws/resources.ts
@@ -1,75 +1,75 @@
 export type AwsResources = {
-    AWSTemplateFormatVersion?: string;
+    AWSTemplateFormatVersion?: string
     Conditions?: {
-      [k: string]: unknown;
-    };
-    Description?: string;
+        [k: string]: unknown
+    }
+    Description?: string
     Mappings?: {
-      [k: string]: unknown;
-    };
+        [k: string]: unknown
+    }
     Metadata?: {
-      [k: string]: unknown;
-    };
+        [k: string]: unknown
+    }
     Outputs?: {
-      [k: string]: unknown;
-    };
+        [k: string]: unknown
+    }
     Parameters?: {
-      [k: string]: unknown;
-    };
+        [k: string]: unknown
+    }
     Resources?: {
-      "Fn::Transform"?: {
-        Name: string;
-        Parameters?: {
-          [k: string]: unknown;
-        };
-      };
-      /**
-       * This interface was referenced by `undefined`'s JSON-Schema definition
-       * via the `patternProperty` "^[a-zA-Z0-9]{1,255}$".
-       */
-      [k: string]: {
-        Type: string;
-        Properties?: {
-          [k: string]: unknown;
-        };
-        CreationPolicy?: {
-          [k: string]: unknown;
-        };
-        DeletionPolicy?: string;
-        DependsOn?: AwsResourceDependsOn;
-        Metadata?: {
-          [k: string]: unknown;
-        };
-        UpdatePolicy?: {
-          [k: string]: unknown;
-        };
-        UpdateReplacePolicy?: string;
-        Condition?: AwsResourceCondition;
-      };
-    };
-    Transform?: string[];
+        'Fn::Transform'?: {
+            Name: string
+            Parameters?: {
+                [k: string]: unknown
+            }
+        }
+        /**
+         * This interface was referenced by `undefined`'s JSON-Schema definition
+         * via the `patternProperty` "^[a-zA-Z0-9]{1,255}$".
+         */
+    [k: string]: {
+            Type: string
+            Properties?: {
+                [k: string]: unknown
+            }
+            CreationPolicy?: {
+                [k: string]: unknown
+            }
+            DeletionPolicy?: string
+            DependsOn?: AwsResourceDependsOn
+            Metadata?: {
+                [k: string]: unknown
+            }
+            UpdatePolicy?: {
+                [k: string]: unknown
+            }
+            UpdateReplacePolicy?: string
+            Condition?: AwsResourceCondition
+        }
+    }
+    Transform?: string[]
     extensions?: {
-      /**
-       * This interface was referenced by `undefined`'s JSON-Schema definition
-       * via the `patternProperty` "^[a-zA-Z0-9]{1,255}$".
-       */
-      [k: string]: {
-        Properties?: {
-          [k: string]: unknown;
-        };
-        CreationPolicy?: {
-          [k: string]: unknown;
-        };
-        DeletionPolicy?: string;
-        DependsOn?: AwsResourceDependsOn;
-        Metadata?: {
-          [k: string]: unknown;
-        };
-        UpdatePolicy?: {
-          [k: string]: unknown;
-        };
-        UpdateReplacePolicy?: string;
-        Condition?: AwsResourceCondition;
-      };
-    };
-  };
+        /**
+         * This interface was referenced by `undefined`'s JSON-Schema definition
+         * via the `patternProperty` "^[a-zA-Z0-9]{1,255}$".
+         */
+        [k: string]: {
+            Properties?: {
+                [k: string]: unknown
+            }
+            CreationPolicy?: {
+                [k: string]: unknown
+            }
+            DeletionPolicy?: string
+            DependsOn?: AwsResourceDependsOn
+            Metadata?: {
+                [k: string]: unknown
+            }
+            UpdatePolicy?: {
+                [k: string]: unknown
+            }
+            UpdateReplacePolicy?: string
+            Condition?: AwsResourceCondition
+        }
+    }
+}
